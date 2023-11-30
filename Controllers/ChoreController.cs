345a034362d14b666a3a04c5d78e49ca4c5be7ab
caliz83 <lizg1308@gmail.com>
2023-11-29 21:03:18 -0800

    add endpoints to ChoreController, created items in ChoreItemService (not completed)

--- Controllers/ChoreController.cs
+++ Controllers/ChoreController.cs
@@ -2,14 +2,61 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using NeatStreakBackEnd.Models;
+using NeatStreakBackEnd.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace NeatStreakBackEnd.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     public class ChoreController : ControllerBase
     {
+        //variable to hold data
+        private readonly ChoreItemService _data;
+
+        //constructor
+        public ChoreController(ChoreItemService dataFromService){
+            _data = dataFromService;
+        }
+
+        //ENDPOINTS!!
+        //AddChoreItem
+        [HttpPost("AddChoreItem")]
+        public bool AddChoreItem(ChoreItemModel newChoreItem){
+            return _data.AddChoreItem(newChoreItem);
+        }
+
+        //GetAllChoreItems
+        [HttpGet("GetChoreItems")]
+        public IEnumerable<ChoreItemModel>GetAllChoreItems(){
+            return _data.GetAllChoreItems();
+        }
+
+        //GetChoreItemsByCategory
+        [HttpGet("GetItemsByCategory/{Category}")]
+        public IEnumerable<ChoreItemModel>GetItemsByCategory(string Category){
+            return _data.GetItemsByCategory(Category);
+        }
+
+        //GetChoreItemByDueDate
+        [HttpGet("GetItemsByDueDate/{Date}")]
+        public IEnumerable<ChoreItemModel>GetItemsByDueDate(string Date){
+            return _data.GetItemsByDueDate(Date);
+        }
+
+        //GetCompletedChoreItems (?)
+        [HttpGet("GetCompletedChoreItems")]
+        public IEnumerable<ChoreItemModel>GetCompletedChoreItems(){
+            return _data.GetCompletedChoreItems();
+        }
+
+        //DeleteChoreItem (?)
+        [HttpPost("DeleteChoreItem/{ChoreItemToDelete}")]
+        public bool DeleteChoreItem(ChoreItemModel ChoreDelete){
+            return _data.DeleteChoreItem(ChoreDelete);
+        }
+
         
     }
 }
--- Services/ChoreItemService.cs
+++ Services/ChoreItemService.cs
@@ -2,11 +2,40 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using NeatStreakBackEnd.Models;
 
 namespace NeatStreakBackEnd.Services
 {
     public class ChoreItemService
     {
-        
+        internal bool AddChoreItem(ChoreItemModel newChoreItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal bool DeleteChoreItem(ChoreItemModel choreDelete)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal IEnumerable<ChoreItemModel> GetAllChoreItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal IEnumerable<ChoreItemModel> GetCompletedChoreItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal IEnumerable<ChoreItemModel> GetItemsByCategory(string category)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal IEnumerable<ChoreItemModel> GetItemsByDueDate(string date)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
